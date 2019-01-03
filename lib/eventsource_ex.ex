4701f301f774fca31ac94d8a703434ec81adea13
Bryan Tidwell <btidwell@atiba.com>
2019-01-03 17:28:01 -0600

    add follow redirect option

--- lib/eventsource_ex.ex
+++ lib/eventsource_ex.ex
@@ -23,9 +23,10 @@ defmodule EventsourceEx do
     url = opts[:url]
     headers = opts[:headers]
     parent = opts[:stream_to]
+    follow_redirect = opts[:follow_redirect]
     ssl = optional(opts[:ssl])
 
-    HTTPoison.get!(url, headers, stream_to: self(), ssl: ssl, recv_timeout: :infinity)
+    HTTPoison.get!(url, headers, [stream_to: self(), ssl: ssl, follow_redirect: follow_redirect, recv_timeout: :infinity])
 
     {:ok, %{parent: parent, message: %EventsourceEx.Message{}, prev_chunk: nil}}
   end
